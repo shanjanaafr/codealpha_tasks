8f435ae047228385ae0d9daf0971d2a251b970e0
shanjanaafr <shanjanaamuniraj@gmail.com>
2026-05-14 12:42:05 +0530

    Create chatbox.py

---
+++ chatbox.py
@@ -0,0 +1,22 @@
+print("🤖 Simple Chatbot")
+print("Type 'bye' to exit\n")
+
+while True:
+
+    user = input("You: ").lower()
+
+    if user == "hello":
+        print("Bot: Hi!")
+
+    elif user == "how are you":
+        print("Bot: I'm fine, thanks!")
+
+    elif user == "what is your name":
+        print("Bot: I'm a Python chatbot.")
+
+    elif user == "bye":
+        print("Bot: Goodbye!")
+        break
+
+    else:
+        print("Bot: Sorry, I don't understand.")
